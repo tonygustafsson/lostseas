fe7ebdad39ad62e62b5cfb9a25dca87d87f16144
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-04-28 07:25:33 +0200

    Auth moving forward

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,67 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { compare, hash } from "bcrypt";
+import { GraphQLClient, gql } from "graphql-request";
+
+const client = new GraphQLClient(process.env.HYGRAPH_ENDPOINT || "", {
+  headers: {
+    Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`,
+  },
+});
+
+const GetUserByEmail = gql`
+  query GetUserByEmail($email: String!) {
+    user: nextUser(where: { email: $email }, stage: DRAFT) {
+      id
+      password
+    }
+  }
+`;
+
+const CreateNextUserByEmail = gql`
+  mutation CreateNextUserByEmail($email: String!, $password: String!) {
+    newUser: createNextUser(data: { email: $email, password: $password }) {
+      id
+    }
+  }
+`;
 
 export default NextAuth({
   providers: [
     CredentialsProvider({
       name: "Email and Password",
       authorize: async (credentials) => {
-        const user = { id: 1, name: "Jamie" };
-        return null;
+        const { user } = (await client.request(GetUserByEmail, {
+          email: credentials?.email,
+        })) as any;
+
+        if (!user) {
+          const { newUser } = (await client.request(CreateNextUserByEmail, {
+            email: credentials?.email,
+            password: await hash(credentials?.password || "", 12),
+          })) as any;
+
+          return {
+            id: newUser.id,
+            username: credentials?.email,
+            email: credentials?.email,
+          };
+        }
+
+        const isValid = await compare(
+          credentials?.password || "",
+          user.password
+        );
+
+        if (!isValid) {
+          throw new Error("Wrong credentials. Try again.");
+        }
+
+        return {
+          id: user.id,
+          username: credentials?.email,
+          email: credentials?.email,
+        };
       },
       credentials: {
         email: {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { useSession, signIn, signOut } from "next-auth/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+function AuthLinks() {
+  const { data: session, status } = useSession();
+
+  const loading = status === "loading";
+
+  if (loading) return null;
+
+  return (
+    <>
+      {session ? (
+        <p>
+          <span>Signed in as {session?.user?.email}</span>
+          <button onClick={() => signOut()}>Sign out</button>
+        </p>
+      ) : (
+        <>
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+    </>
+  );
+}
 
 export default function Home() {
   return (
@@ -20,7 +44,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -45,74 +69,8 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <AuthLinks />
       </div>
     </main>
-  )
+  );
 }
