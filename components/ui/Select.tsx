0d3dd478561dd2c66c0bbac6660f20df2a8eb988
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-05-05 08:51:10 +0200

    React fixes

--- components/ui/Select.tsx
+++ components/ui/Select.tsx
@@ -29,15 +29,12 @@ const Select = ({
       <select
         id={id}
         name={name}
+        value={value}
         onChange={(e) => onChange?.(e.target.value)}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-[11px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
         {options.map((option) => (
-          <option
-            key={`select-option-${option}`}
-            value={option}
-            selected={value === option}
-          >
+          <option key={`select-option-${option}`} value={option}>
             {option}
           </option>
         ))}
--- components/ui/Table.tsx
+++ components/ui/Table.tsx
@@ -9,8 +9,13 @@ const Table = ({ headings, rows }: Props) => {
       <table className="table text-gray-400 border-separate  space-y-6 text-sm w-1/2">
         <thead className="bg-gray-900 text-gray-300">
           <tr>
-            {headings.map((heading) => (
-              <th className="text-left p-1.5">{heading}</th>
+            {headings.map((heading, idx) => (
+              <th
+                key={`table-heading-${heading}-${idx}`}
+                className="text-left p-1.5"
+              >
+                {heading}
+              </th>
             ))}
           </tr>
         </thead>
@@ -18,11 +23,13 @@ const Table = ({ headings, rows }: Props) => {
         <tbody>
           {rows.map((row, idx) => (
             <tr
-              key={`ship-${idx}`}
+              key={`table-row-${row.flatMap((column) => column[0])}-${idx}`}
               className={idx % 2 !== 0 ? "bg-gray-900" : "bg-gray-800"}
             >
-              {row.map((column) => (
-                <td className="p-1.5">{column}</td>
+              {row.map((column, idx) => (
+                <td key={`table-column-${column}-${idx}`} className="p-1.5">
+                  {column}
+                </td>
               ))}
             </tr>
           ))}
