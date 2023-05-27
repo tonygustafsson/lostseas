347f61eec80fe1be3edc5e7f22487994d3fdd9a1
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-05-27 09:15:26 +0200

    Simplify hook

--- hooks/queries/useCharacter.ts
+++ hooks/queries/useCharacter.ts
@@ -2,50 +2,45 @@ import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 import { PLAYER_QUERY_KEY } from "./usePlayer"
 
+const apiRequest = async (url: string, data: any) => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+
+    if (!response.ok) {
+      console.error(`An error occurred while fetching the data from ${url}`)
+    }
+
+    const result = await response.json()
+
+    return result
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 export const useCharacterMutations = () => {
   const queryClient = useQueryClient()
 
   const { mutate: travel, isLoading: travelIsLoading } = useMutation(
-    async (data: { userId: Player["id"]; town: Town }) => {
-      const inputData = await fetch("/api/character/travel", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      const result = await inputData.json()
-
-      return result
-    },
+    (data: { userId: Player["id"]; town: Town }) =>
+      apiRequest("/api/character/travel", data),
     {
-      onSuccess: () => {
-        queryClient.invalidateQueries([PLAYER_QUERY_KEY])
-      },
+      onSuccess: () => queryClient.invalidateQueries([PLAYER_QUERY_KEY]),
       onError: (error) => console.error(error),
     }
   )
 
   const { mutate: move, isLoading: isMoving } = useMutation(
-    async (data: {
-      userId: Player["id"]
-      location: TownLocation | SeaLocation
-    }) => {
-      const inputData = await fetch("/api/character/move", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      const result = await inputData.json()
-
-      return result
-    },
+    (data: { userId: Player["id"]; location: TownLocation | SeaLocation }) =>
+      apiRequest("/api/character/move", data),
     {
-      onSuccess: () => {
-        queryClient.invalidateQueries([PLAYER_QUERY_KEY])
-      },
+      onSuccess: () => queryClient.invalidateQueries([PLAYER_QUERY_KEY]),
       onError: (error) => console.error(error),
     }
   )
