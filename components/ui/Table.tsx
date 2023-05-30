97f78276744e95d1bcf6dbca7d6fde341401e395
Tony Gustafsson <mail@tonyg.se>
2023-05-30 21:59:12 +0200

    New tables

--- components/ui/Table.tsx
+++ components/ui/Table.tsx
@@ -4,17 +4,12 @@ type Props = {
 }
 
 const Table = ({ headings, rows }: Props) => (
-  <div className="overflow-auto lg:overflow-visible">
-    <table className="table text-gray-400 border-separate  space-y-6 text-sm w-1/2">
-      <thead className="bg-gray-900 text-gray-300">
+  <div className="overflow-x-auto">
+    <table className="table table-zebra w-full">
+      <thead>
         <tr>
           {headings.map((heading, idx) => (
-            <th
-              key={`table-heading-${heading}-${idx}`}
-              className="text-left p-1.5"
-            >
-              {heading}
-            </th>
+            <th key={`table-heading-${heading}-${idx}`}>{heading}</th>
           ))}
         </tr>
       </thead>
@@ -25,12 +20,9 @@ const Table = ({ headings, rows }: Props) => (
             key={`table-row-${row.flatMap((column) =>
               column?.toString()
             )}-${idx}`}
-            className={idx % 2 !== 0 ? "bg-gray-900" : "bg-gray-800"}
           >
             {row.map((column, idx) => (
-              <td key={`table-column-${column}-${idx}`} className="p-1.5">
-                {column}
-              </td>
+              <td key={`table-column-${column}-${idx}`}>{column}</td>
             ))}
           </tr>
         ))}
