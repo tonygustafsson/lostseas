e7f65f44800eed105697e35c232ad84983a97636
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-06-02 11:03:05 +0200

    Fix logged out state

---
+++ components/LoggedOutHero.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link"
+
+import { useGetPlayer } from "@/hooks/queries/usePlayer"
+
+const LoggedOutHero = () => {
+  const { data: player } = useGetPlayer()
+
+  if (player) return null
+
+  return (
+    <div className="hero rounded-lg">
+      <div className="hero-overlay bg-opacity-50"></div>
+      <div className="hero-content text-center text-neutral-content py-24">
+        <div className="max-w-2xl bg-base-300 bg-opacity-60 p-8 rounded-lg">
+          <h1 className="font-serif mb-5 text-5xl">Lost Seas</h1>
+
+          <p className="mb-5">Please login or register to start playing.</p>
+
+          <div className="flex gap-4 mt-4 justify-center">
+            <Link href="/login">
+              <button className="btn btn-primary">Sign in</button>
+            </Link>
+
+            <Link href="/register">
+              <button className="btn btn-primary">Register</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoggedOutHero
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,12 @@
-import Link from "next/link"
-
 import DefaultLayout from "@/components/layouts/default"
 import LocationHero from "@/components/LocationHero"
-import { useGetPlayer } from "@/hooks/queries/usePlayer"
+import LoggedOutHero from "@/components/LoggedOutHero"
 
-const Home = () => {
-  const { data: player } = useGetPlayer()
-
-  return (
-    <DefaultLayout>
-      <LocationHero />
-
-      {!player && (
-        <>
-          <h1 className="font-serif text-4xl mb-8">Signed out</h1>
-
-          <div className="flex gap-4">
-            <Link href="/login">
-              <button className="btn btn-primary btn-lg">Sign in</button>
-            </Link>
-
-            <Link href="/register">
-              <button className="btn btn-primary btn-lg">Register</button>
-            </Link>
-          </div>
-        </>
-      )}
-    </DefaultLayout>
-  )
-}
+const Home = () => (
+  <DefaultLayout>
+    <LocationHero />
+    <LoggedOutHero />
+  </DefaultLayout>
+)
 
 export default Home
