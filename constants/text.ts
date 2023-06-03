4c915074f2880e8339c875e713e10a43cc124a50
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-06-03 10:04:29 +0200

    Add some AI descriptive texts

--- constants/text.ts
+++ constants/text.ts
@@ -1,13 +1,17 @@
 export const LOCATION_DESCRIPTION = {
   Harbor:
     "You can clearly see the town ahead of you. The harbor is busy with people and ships. You can see the city hall, the tavern, the shipyard, the market, the bank, and the shop.",
-  Sea: "You are on open waters now. You can hear the seagulls and the waves below you.",
-  Tavern: "You are in the tavern.",
-  Shipyard: "You are in the shipyard.",
-  Market: "You are in the market.",
-  Bank: "You are in the bank.",
-  "City hall": "You are in the town hall.",
-  Shop: "You are in the shop.",
+  Sea: "Sailing the open sea on a pirate ship is an exhilarating dance with the elements. As the salty breeze whips through your hair and the waves crash against the hull, the vast expanse of the ocean becomes both your playground and your battleground.",
+  Tavern:
+    "The tavern stands as a dimly lit den of debauchery and camaraderie. The air is thick with the scent of rum and tobacco, echoing with boisterous laughter and raucous tales of daring exploits, creating an atmosphere where pirates find solace and forge their alliances.",
+  Shipyard:
+    "The shipyard is a bustling hub of activity, where skilled craftsmen and swashbuckling captains come together to breathe life into their nautical dreams. Echoes of hammering and sawing fill the air as mighty vessels take shape, standing as testaments to the ingenuity and ambition of those who call the sea their home.",
+  Market:
+    "The pirate market is a vibrant and chaotic spectacle, teeming with a colorful array of stolen treasures, exotic goods, and dubious merchandise. The air is filled with the calls of merchants and the haggling of buyers, as fortunes change hands amidst the atmosphere of daring deals and hidden treasures.",
+  Bank: "The bank is an imposing fortress of ill-gotten wealth, guarded by a crew of loyal and ruthless enforcers. Within its secure walls, chests overflowing with stolen gold and precious gems are meticulously cataloged, while secret ledgers document the intricate web of pirate transactions, ensuring that no treasure goes unaccounted for.",
+  "City hall":
+    "The city hall stands as a grand and imposing structure, adorned with tattered flags and weather-worn banners. Within its walls, pirate lords convene, plotting their territorial disputes and forging alliances amidst an atmosphere of intrigue and whispered schemes.",
+  Shop: "The shop is a treasure trove of exotic and illicit wares, its shelves laden with gleaming cutlasses, intricate navigational instruments, and maps marked with hidden routes. The air is heavy with the scent of gunpowder and the whispers of whispered tales, as pirates browse the wares, seeking tools of their trade and the key to their next adventure.",
   Docks:
-    "The harbor smells like fish and you see numerous ships. Some are small and some are large frigates. The people here seems to work hard and does not pay attention to you.",
+    "The docks of the town emanate an aura of rugged allure, where weathered planks bear the weight of countless voyages and the salty sea breeze carries tales of daring escapades. Amidst a vibrant mix of characters, weather-beaten pirates with faces etched in hardship converse in hushed tones, while the clanging of metal and distant shouts of captains create an atmosphere of organized chaos.",
 } as Record<SeaLocation | TownLocation, string>
