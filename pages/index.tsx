4b7a50ec3cdc22ffc35c36e013d0a5d7b844fe4a
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-06-11 17:29:04 +0200

    RQ fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { dehydrate, QueryClient } from "@tanstack/react-query"
-import { getCookie } from "cookies-next"
+import { deleteCookie, getCookie } from "cookies-next"
 import { GetServerSideProps } from "next"
 
 import DefaultLayout from "@/components/layouts/default"
@@ -9,7 +9,7 @@ import Shop from "@/components/location/Shop"
 import LocationHero from "@/components/LocationHero"
 import LoggedOutHero from "@/components/LoggedOutHero"
 import { PLAYER_ID_COOKIE_NAME } from "@/constants/system"
-import { useGetPlayer } from "@/hooks/queries/usePlayer"
+import { PLAYER_QUERY_KEY, useGetPlayer } from "@/hooks/queries/usePlayer"
 
 const Home = () => {
   const { data: player } = useGetPlayer()
@@ -37,6 +37,26 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   })
 
   console.log(playerId)
+
+  if (playerId) {
+    await queryClient.prefetchQuery([PLAYER_QUERY_KEY], async () => {
+      try {
+        // TODO: Do what API does directly
+        const res = await fetch(`/api/user/get/${playerId}`)
+
+        if (res.status !== 200) {
+          deleteCookie(PLAYER_ID_COOKIE_NAME)
+          window.location.href = "/"
+          return
+        }
+
+        const data = (await res.json()) as Player
+        return data
+      } catch (error) {
+        console.error(error)
+      }
+    })
+  }
 
   return {
     props: {
