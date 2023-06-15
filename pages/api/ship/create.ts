2257b1728316097af1478b7b0482ddd9c91e17aa
Tony Gustafsson <mail@tonyg.se>
2023-06-15 22:48:28 +0200

    Broken create

--- pages/api/ship/create.ts
+++ pages/api/ship/create.ts
@@ -2,12 +2,12 @@ import { child, get, ref, set } from "firebase/database"
 import { NextApiRequest, NextApiResponse } from "next/types"
 
 import db from "@/firebase/db"
-import createNewShips from "@/utils/createNewShips"
+import createNewShip from "@/utils/createNewShip"
 
 const createShip = async (req: NextApiRequest, res: NextApiResponse) => {
   const dbRef = ref(db)
   const playerId = req.body.playerId
-  const ship = createNewShips(1, req.body.type)
+  const ship = createNewShip(req.body.type)
 
   const existingShips = await get(child(dbRef, `${playerId}/ships`))
   const result = existingShips.val()
