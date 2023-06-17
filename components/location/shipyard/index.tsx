280ec275bbd035a43f11b267f7429f8dbdb6a5df
Tony Gustafsson <mail@tonyg.se>
2023-06-17 14:23:27 +0200

    Add tabs to shipyard

--- components/location/shipyard/index.tsx
+++ components/location/shipyard/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import MerchandiseCard from "@/components/MerchandiseCard"
 import MerchandiseIcon from "@/components/MerchandiseIcon"
 import { SHIP_TYPES } from "@/constants/ship"
@@ -7,6 +9,8 @@ import { useShipyard } from "@/hooks/queries/useShipyard"
 const Shipyard = () => {
   const { data: player } = useGetPlayer()
   const { buy, sell } = useShipyard()
+
+  const [tab, setTab] = useState<"Buy" | "Sell" | "Repair">("Buy")
 
   const handleBuy = (item: keyof typeof SHIP_TYPES) => {
     buy({ playerId: player?.id || "", item })
@@ -18,69 +22,108 @@ const Shipyard = () => {
 
   return (
     <>
-      <div className="flex flex-wrap gap-6">
-        {Object.entries(SHIP_TYPES).map(([shipType, { description, buy }]) => (
-          <MerchandiseCard
-            key={`shipyard-buy-${shipType}`}
-            title={shipType}
-            //indicator={player?.inventory[inventoryItem]?.toString() || "0"}
-            icon={<MerchandiseIcon size="lg" item={shipType} />}
-            body={
-              <>
-                <p>{description}</p>
-
-                <div className="flex gap-2">
-                  <div className="badge badge-secondary">Price: {buy} dbl</div>
-                </div>
-              </>
-            }
-            actions={
-              <button
-                className="btn btn-primary btn-sm"
-                onClick={() => handleBuy(shipType as keyof typeof SHIP_TYPES)}
-              >
-                Buy
-              </button>
-            }
-          />
-        ))}
+      <div className="tabs justify-center mb-8">
+        <a
+          className={`tab tab-lg text-2xl tab-bordered font-bold px-8 ${
+            tab === "Buy" ? "tab-active" : ""
+          }`}
+          onClick={() => setTab("Buy")}
+        >
+          Buy
+        </a>
+        <a
+          className={`tab tab-lg text-2xl tab-bordered font-bold px-8 ${
+            tab === "Sell" ? "tab-active" : ""
+          }`}
+          onClick={() => setTab("Sell")}
+        >
+          Sell
+        </a>
+        <a
+          className={`tab tab-lg text-2xl tab-bordered font-bold px-8 ${
+            tab === "Repair" ? "tab-active" : ""
+          }`}
+          onClick={() => setTab("Repair")}
+        >
+          Repair
+        </a>
       </div>
 
-      <div className="flex flex-wrap gap-6 mt-8">
-        {Object.entries(player?.ships || []).map(([id, { name, type }]) => {
-          const shipInfo = SHIP_TYPES[type as keyof typeof SHIP_TYPES]
+      {tab === "Buy" && (
+        <div className="flex flex-wrap gap-6">
+          {Object.entries(SHIP_TYPES).map(
+            ([shipType, { description, buy }]) => (
+              <MerchandiseCard
+                key={`shipyard-buy-${shipType}`}
+                title={shipType}
+                //indicator={player?.inventory[inventoryItem]?.toString() || "0"}
+                icon={<MerchandiseIcon size="lg" item={shipType} />}
+                body={
+                  <>
+                    <p>{description}</p>
 
-          if (!shipInfo) return null
-
-          return (
-            <MerchandiseCard
-              key={`shipyard-sell-${name}`}
-              title={name}
-              //indicator={player?.inventory[inventoryItem]?.toString() || "0"}
-              icon={<MerchandiseIcon size="lg" item={type} />}
-              body={
-                <>
-                  <p>{shipInfo.description}</p>
-
-                  <div className="flex gap-2">
-                    <div className="badge badge-secondary">
-                      Price: {shipInfo.sell} dbl
+                    <div className="flex gap-2">
+                      <div className="badge badge-secondary">
+                        Price: {buy} dbl
+                      </div>
                     </div>
-                  </div>
-                </>
-              }
-              actions={
-                <button
-                  className="btn btn-primary btn-sm"
-                  onClick={() => handleSell(id)}
-                >
-                  Sell {type}
-                </button>
-              }
-            />
-          )
-        })}
-      </div>
+                  </>
+                }
+                actions={
+                  <button
+                    className="btn btn-primary btn-sm"
+                    onClick={() =>
+                      handleBuy(shipType as keyof typeof SHIP_TYPES)
+                    }
+                  >
+                    Buy
+                  </button>
+                }
+              />
+            )
+          )}
+        </div>
+      )}
+
+      {tab === "Sell" && (
+        <div className="flex flex-wrap gap-6 mt-8">
+          {Object.entries(player?.ships || []).map(([id, { name, type }]) => {
+            const shipInfo = SHIP_TYPES[type as keyof typeof SHIP_TYPES]
+
+            if (!shipInfo) return null
+
+            return (
+              <MerchandiseCard
+                key={`shipyard-sell-${name}`}
+                title={name}
+                //indicator={player?.inventory[inventoryItem]?.toString() || "0"}
+                icon={<MerchandiseIcon size="lg" item={type} />}
+                body={
+                  <>
+                    <p>{shipInfo.description}</p>
+
+                    <div className="flex gap-2">
+                      <div className="badge badge-secondary">
+                        Price: {shipInfo.sell} dbl
+                      </div>
+                    </div>
+                  </>
+                }
+                actions={
+                  <button
+                    className="btn btn-primary btn-sm"
+                    onClick={() => handleSell(id)}
+                  >
+                    Sell {type}
+                  </button>
+                }
+              />
+            )
+          })}
+        </div>
+      )}
+
+      {tab === "Repair" && <p>Not implemented yet.</p>}
     </>
   )
 }
