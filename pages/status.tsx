dcc9e19b1e6c1ef7f64498e62bbe7bd6de59f2e1
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-06-19 07:24:06 +0200

    Update status page

--- pages/status.tsx
+++ pages/status.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from "next"
 import Link from "next/link"
+import { BiFemaleSign, BiMaleSign, BiTime } from "react-icons/bi"
 import { GiCoins } from "react-icons/gi"
 
 import ChangeCharacterForm from "@/components/ChangeCharacterForm"
@@ -31,19 +32,47 @@ const Status = () => {
 
       <div className="card w-full bg-gray-800 shadow-lg mt-8 rounded-md">
         <div className="card-body p-6">
-          <h2 className="card-title">
-            <Flag
-              nation={player?.character.nationality}
-              size={24}
-              className="opacity-[0.8]"
-            />
+          <h2 className="card-title text-3xl font-normal font-serif">
             {player?.character.name}
           </h2>
-          <p>
-            You are a {player?.character.age} year old{" "}
-            {player?.character.gender.toLowerCase()} from{" "}
-            {player?.character.nationality}.
-          </p>
+
+          <div className="stats gap-4 mt-4">
+            <div className="stat bg-gray-700">
+              <div className="stat-figure text-secondary">
+                <Flag
+                  nation={player?.character.nationality}
+                  size={32}
+                  className="opacity-[0.8]"
+                />
+              </div>
+              <div className="stat-title">Nationality</div>
+              <div className="stat-value text-2xl">
+                {player?.character.nationality}
+              </div>
+            </div>
+
+            <div className="stat bg-gray-700">
+              <div className="stat-figure text-secondary">
+                <BiTime className="h-8 w-8" />
+              </div>
+              <div className="stat-title">Age</div>
+              <div className="stat-value text-2xl">{player?.character.age}</div>
+            </div>
+
+            <div className="stat bg-gray-700">
+              <div className="stat-figure text-secondary">
+                {player.character.gender === "Male" ? (
+                  <BiMaleSign className="h-8 w-8" />
+                ) : (
+                  <BiFemaleSign className="h-8 w-8" />
+                )}
+              </div>
+              <div className="stat-title">Gender</div>
+              <div className="stat-value text-2xl">
+                {player?.character.gender}
+              </div>
+            </div>
+          </div>
 
           <div className="stats gap-4 mt-4">
             <div className="stat bg-gray-700">
