8e39598c401386ec8c7a7531c0b09eb758eef72c
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-06-19 07:56:23 +0200

    Starting tavern meet sailors

---
+++ components/ActionCard.tsx
@@ -0,0 +1,24 @@
+type Props = {
+  title?: string
+  message: string
+  actions?: React.ReactNode
+  icon?: React.ReactNode
+}
+
+const ActionCard = ({ title, message, actions, icon }: Props) => (
+  <div className="hero mb-8 bg-gray-800 border border-gray-700 rounded-lg">
+    <div className="hero-content max-w-none w-full items-start justify-start flex-col lg:flex-row p-8 gap-8">
+      {icon}
+
+      <div className="max-w-3xl">
+        <h1 className="text-2xl font-bold">{title}</h1>
+
+        <p className="pt-4">{message}</p>
+
+        {actions && <div className="flex gap-4 pt-6">{actions}</div>}
+      </div>
+    </div>
+  </div>
+)
+
+export default ActionCard
--- components/location/Tavern/index.tsx
+++ components/location/Tavern/index.tsx
@@ -1,3 +1,6 @@
+import { GiPirateCaptain } from "react-icons/gi"
+
+import ActionCard from "@/components/ActionCard"
 import { TAVERN_ITEMS } from "@/constants/tavern"
 import { useGetPlayer } from "@/hooks/queries/usePlayer"
 
@@ -7,15 +10,33 @@ const Tavern = () => {
   const { data: player } = useGetPlayer()
 
   return (
-    <div className="flex flex-wrap gap-6">
-      {Object.keys(TAVERN_ITEMS).map((item) => (
-        <ShopItem
-          key={`tavern-item-${item}`}
-          item={item as keyof typeof TAVERN_ITEMS}
-          player={player}
-        />
-      ))}
-    </div>
+    <>
+      <ActionCard
+        title="A bunch of sailors approach you"
+        message="Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi."
+        icon={<GiPirateCaptain className="w-20 h-20 text-secondary" />}
+        actions={
+          <>
+            <button className="btn btn-primary">Talk to them</button>
+            <button className="btn btn-secondary">Ignore them</button>
+          </>
+        }
+      />
+
+      <h2 className="text-3xl font-serif mb-8">Buy</h2>
+
+      <div className="flex flex-wrap gap-6">
+        {Object.keys(TAVERN_ITEMS).map((item) => (
+          <ShopItem
+            key={`tavern-item-${item}`}
+            item={item as keyof typeof TAVERN_ITEMS}
+            player={player}
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
