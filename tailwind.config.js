f8a6b706e219c4086e16d0ee33cdd64ae0154ee4
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-06-22 16:56:12 +0200

    Make SB fonts work

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./stories/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
