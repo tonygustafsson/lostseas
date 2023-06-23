c045c2e5e38e049ea05c87a4777b546b5d844cdd
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-06-23 11:58:19 +0200

    Dice design done

--- components/location/Tavern/Dice.tsx
+++ components/location/Tavern/Dice.tsx
@@ -1,31 +1,44 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import { useGetPlayer } from "@/hooks/queries/usePlayer"
 
 export type TavernTab = "Buy" | "Dice"
 
-const betValues = [5, 10, 25, 50, 75, 100]
-const defaultBet = 10
+const BET_VALUES = [5, 10, 25, 50, 75, 100]
+const BET_DEFAULT = 10
+const PROFIT_MIN = 2
+const PROFIT_MAX = 5
+const PROFIT_JACKPOT = 20
+
+const getBet = (percentage: number, doubloons: number) => {
+  if (percentage === 100) return doubloons
+
+  return Math.floor((percentage / 100) * doubloons)
+}
 
 const TavernDice = () => {
   const { data: player } = useGetPlayer()
 
-  const [betPercentage, setBetPercentage] = useState(defaultBet)
-  const [bet, setBet] = useState(0)
+  const [betPercentage, setBetPercentage] = useState(BET_DEFAULT)
+  const [bet, setBet] = useState(
+    getBet(BET_DEFAULT, player?.character.doubloons || 0)
+  )
 
-  const isEnabled = (player?.character.doubloons || 0) >= betPercentage
+  useEffect(() => {
+    setBet(getBet(betPercentage, player?.character.doubloons || 0))
+  }, [betPercentage, player?.character.doubloons])
+
+  const profitMin = Math.floor(bet * PROFIT_MIN)
+  const profitMax = Math.floor(bet * PROFIT_MAX)
+  const profitJackpot = Math.floor(bet * PROFIT_JACKPOT)
+
+  const disabled = bet > (player?.character.doubloons || 0)
 
   return (
-    <>
-      <p>Play dice and get a chance of winning some money.</p>
-      <p>
-        You have 1/6 chance to win. If you get really lucky you will win the
-        huge Jackpot.
-      </p>
-
-      <div className="flex flex-col items-center">
-        <div className="flex flex-row join my-8">
-          {betValues.map((value) => (
+    <div className="flex flex-col items-center">
+      <div className="flex flex-row join mb-8">
+        <div className="flex items-center">
+          {BET_VALUES.map((value) => (
             <button
               key={`tavern-dice-bet-${value}`}
               className={`btn join-item ${
@@ -33,14 +46,23 @@ const TavernDice = () => {
               }`}
               onClick={() => setBetPercentage(value)}
             >
-              Bet {value === 100 ? "All" : `${value}%`}
+              Bet {value === 100 ? "all" : `${value}%`}
             </button>
           ))}
-        </div>
 
-        <button className="btn btn-lg btn-primary">Play</button>
+          <button className="btn btn-lg btn-primary ml-4" disabled={disabled}>
+            Play
+          </button>
+        </div>
       </div>
-    </>
+
+      <p className="text-lg font-serif">You will bet {bet} doubloons</p>
+
+      <p className="text-sm mt-2">
+        You have 1/6 change of winning between {profitMin} and {profitMax}{" "}
+        doubloons. If you hit jackpot you would get {profitJackpot} doubloons.
+      </p>
+    </div>
   )
 }
 
