a2ab01849d60b201a049a37f49b89131550907ad
Tony Gustafsson <mail@tonyg.se>
2023-06-24 22:46:08 +0200

    Starting with giving crew stuff to feel good

--- pages/crew.tsx
+++ pages/crew.tsx
@@ -1,20 +1,59 @@
 import { GetServerSideProps } from "next"
+import { useState } from "react"
 import { BsHeartPulseFill } from "react-icons/bs"
 import { FaUsers } from "react-icons/fa"
 import { TbMoodSmileBeam } from "react-icons/tb"
 
 import DefaultLayout from "@/components/layouts/default"
+import MerchandiseCard from "@/components/MerchandiseCard"
+import MerchandiseIcon from "@/components/MerchandiseIcon"
+import TextField from "@/components/ui/TextField"
+import { MERCHANDISE } from "@/constants/merchandise"
 import { useGetPlayer } from "@/hooks/queries/usePlayer"
 import { getLoggedInServerSideProps } from "@/utils/next/getLoggedInServerSideProps"
 
+type Item = "medicine" | "doubloons"
+
 const Crew = () => {
   const { data: player } = useGetPlayer()
+
+  const [medicineQuantity, setMedicineQuantity] = useState(0)
+  const [doubloonsQuantity, setDoubloonsQuantity] = useState(0)
+
+  const changeQuantity = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    item: Item
+  ) => {
+    const setQuantity =
+      item === "medicine" ? setMedicineQuantity : setDoubloonsQuantity
+
+    if (e.target.value === "") {
+      setQuantity(1)
+    } else {
+      setQuantity(parseInt(e.target.value))
+    }
+  }
+
+  const increase = (item: Item) => {
+    const setQuantity =
+      item === "medicine" ? setMedicineQuantity : setDoubloonsQuantity
+    setQuantity((prev) => prev + 1)
+  }
+
+  const decrease = (item: Item) => {
+    const quantity = item === "medicine" ? medicineQuantity : doubloonsQuantity
+    const setQuantity =
+      item === "medicine" ? setMedicineQuantity : setDoubloonsQuantity
+
+    if (quantity > 1) {
+      setQuantity((prev) => prev - 1)
+    }
+  }
 
   return (
     <DefaultLayout>
       <>
         <h1 className="text-3xl font-serif text mb-8">Crew members</h1>
-
         <div className="stats bg-transparent gap-2 mt-4">
           <div className="stat bg-gray-700">
             <div className="stat-figure text-secondary">
@@ -45,6 +84,107 @@ const Crew = () => {
               {player?.crewMembers.mood}
             </div>
           </div>
+        </div>
+
+        <div className="flex gap-4 pt-8 justify-center">
+          <MerchandiseCard
+            title="Give medicine"
+            indicator={player?.inventory.medicine?.toString() || "0"}
+            icon={<MerchandiseIcon item="Medicine" />}
+            body={
+              <>
+                <p>{MERCHANDISE.medicine.description}</p>
+                <p>
+                  1 box of medicine will heal one crew member, so the more men
+                  you have the more medicine you need.
+                </p>
+              </>
+            }
+            actions={
+              <>
+                <div className="join">
+                  <button
+                    onClick={() => decrease("medicine")}
+                    className="btn btn-sm btn-primary join-item"
+                  >
+                    -
+                  </button>
+
+                  <TextField
+                    value={medicineQuantity.toString()}
+                    onChange={(e) => changeQuantity(e, "medicine")}
+                    type="number"
+                    name={""}
+                    size="sm"
+                    fullWidth={false}
+                    className={`join-item ${medicineQuantity < 10 && "w-9"} ${
+                      medicineQuantity < 100 && "w-11"
+                    } ${medicineQuantity < 1000 && "w-14"} hide-number-arrows`}
+                  />
+
+                  <button
+                    onClick={() => increase("medicine")}
+                    className="btn btn-sm btn-primary join-item"
+                  >
+                    +
+                  </button>
+                </div>
+
+                <button className="btn btn-primary btn-sm">
+                  Give medicine
+                </button>
+              </>
+            }
+          />
+
+          <MerchandiseCard
+            title="Give doubloons"
+            indicator={player?.character.doubloons.toString() || "0"}
+            icon={<MerchandiseIcon item="Doubloons" />}
+            body={
+              <>
+                <p>
+                  Give some doubloons to your crew members to improve their
+                  mood. More crew members means more doubloons needed.
+                </p>
+              </>
+            }
+            actions={
+              <>
+                <div className="join">
+                  <button
+                    onClick={() => decrease("doubloons")}
+                    className="btn btn-sm btn-primary join-item"
+                  >
+                    -
+                  </button>
+
+                  <TextField
+                    value={doubloonsQuantity.toString()}
+                    onChange={(e) => changeQuantity(e, "doubloons")}
+                    type="number"
+                    name={""}
+                    size="sm"
+                    fullWidth={false}
+                    className={`join-item ${doubloonsQuantity < 10 && "w-9"} ${
+                      doubloonsQuantity < 100 && "w-11"
+                    } ${doubloonsQuantity < 1000 && "w-14"} hide-number-arrows`}
+                  />
+
+                  <button
+                    onClick={() => increase("doubloons")}
+                    className="btn btn-sm btn-primary join-item"
+                  >
+                    +
+                  </button>
+                </div>
+
+                <button className="btn btn-primary btn-sm">
+                  Give doubloons
+                </button>
+              </>
+            }
+          />
         </div>
       </>
     </DefaultLayout>
