73b82878fd3260f6625968bbf7559209b296a1a3
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-06-25 08:33:02 +0200

    New ships view

--- pages/ships.tsx
+++ pages/ships.tsx
@@ -1,8 +1,9 @@
 import { GetServerSideProps } from "next"
-import { FiTrash2 } from "react-icons/fi"
 
 import DefaultLayout from "@/components/layouts/default"
-import Table from "@/components/ui/Table"
+import MerchandiseCard from "@/components/MerchandiseCard"
+import MerchandiseIcon from "@/components/MerchandiseIcon"
+import { SHIP_TYPES } from "@/constants/ship"
 import { useGetPlayer } from "@/hooks/queries/usePlayer"
 import { useShips } from "@/hooks/queries/useShips"
 import { getLoggedInServerSideProps } from "@/utils/next/getLoggedInServerSideProps"
@@ -19,32 +20,53 @@ const Ships = () => {
 
   return (
     <DefaultLayout>
-      <>
-        <h1 className="text-3xl font-serif text mb-8">Ships</h1>
+      <h1 className="text-3xl font-serif text mb-8">Ships</h1>
 
-        {!!Object.values(player?.ships || [])?.length && (
-          <>
-            <Table
-              headings={["Name", "Type", "Created", ""]}
-              rows={Object.values(player?.ships || []).map((ship, idx) => [
-                ship.name,
-                ship.type,
-                `${new Date(ship.createdDate).toLocaleDateString()} ${new Date(
-                  ship.createdDate
-                ).toLocaleTimeString()}`,
-                <button
-                  key={`ship-remove-${idx}`}
-                  className="btn btn-secondary btn-sm ml-auto flex"
-                  onClick={() => handleRemoveShip(ship.id)}
-                  disabled={removingIsLoading}
-                >
-                  <FiTrash2 />
-                </button>,
-              ])}
-            />
-          </>
-        )}
-      </>
+      {!!Object.values(player?.ships || [])?.length && (
+        <div className="flex flex-wrap gap-4">
+          {Object.values(player?.ships || []).map((ship) => {
+            const shipInfo = SHIP_TYPES[ship.type]
+
+            if (!shipInfo) return null
+
+            return (
+              <MerchandiseCard
+                key={`ships-${ship.name}`}
+                title={`${ship.name} (${ship.type})`}
+                icon={<MerchandiseIcon item={ship.type} />}
+                body={
+                  <>
+                    <p>{shipInfo.description}</p>
+
+                    <p>
+                      <strong>Created:</strong>{" "}
+                      {new Date(ship.createdDate).toLocaleDateString()}{" "}
+                      {new Date(ship.createdDate).toLocaleTimeString()}
+                    </p>
+                  </>
+                }
+                actions={
+                  <div className="flex gap-2">
+                    <button
+                      className="btn btn-secondary btn-xs"
+                      onClick={() => handleRemoveShip(ship.id)}
+                      disabled={removingIsLoading}
+                    >
+                      Remove
+                    </button>
+
+                    <button className="btn btn-secondary btn-xs">Rename</button>
+                  </div>
+                }
+              />
+            )
+          })}
+        </div>
+      )}
+
+      {!Object.values(player?.ships || [])?.length && (
+        <p>You do not have any ships currently.</p>
+      )}
     </DefaultLayout>
   )
 }
