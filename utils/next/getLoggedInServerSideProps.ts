c448b03b67205e01f81a30cfebc648a0fe66072e
Tony Gustafsson <mail@tonyg.se>
2023-06-26 16:55:20 +0200

    Reload problems does only occur in dev mode, letting it go

--- utils/next/getLoggedInServerSideProps.ts
+++ utils/next/getLoggedInServerSideProps.ts
@@ -1,21 +1,24 @@
 import { dehydrate, QueryClient } from "@tanstack/react-query"
 import { getCookie } from "cookies-next"
-import { GetServerSideProps } from "next"
+import { IncomingMessage, ServerResponse } from "http"
 
 import { PLAYER_ID_COOKIE_NAME } from "@/constants/system"
 import { PLAYER_QUERY_KEY } from "@/hooks/queries/usePlayer"
 
 import { getPlayer } from "../db/getPlayer"
 
-export const getLoggedInServerSideProps: GetServerSideProps = async (
-  context
-) => {
+export const getLoggedInServerSideProps = async ({
+  req,
+  res,
+}: {
+  req: IncomingMessage
+  res: ServerResponse<IncomingMessage>
+}) => {
   const queryClient = new QueryClient()
 
-  const playerId = getCookie(PLAYER_ID_COOKIE_NAME, {
-    req: context.req,
-    res: context.res,
-  }) as Player["id"] | undefined
+  const playerId = getCookie(PLAYER_ID_COOKIE_NAME, { req, res }) as
+    | Player["id"]
+    | undefined
 
   if (playerId) {
     await queryClient.prefetchQuery([PLAYER_QUERY_KEY], async () => {
