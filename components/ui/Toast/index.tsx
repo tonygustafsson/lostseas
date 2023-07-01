ce67e926aeb694c15bb5d2f2d8572d8b185e81eb
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-07-01 11:42:36 +0200

    Toast mobile fixes

--- components/ui/Toast/index.tsx
+++ components/ui/Toast/index.tsx
@@ -11,19 +11,24 @@ const Toast = () => {
 
   return (
     <>
-      {Object.values(toasts).map((toast) => (
+      {Object.values(toasts).map((toast, idx) => (
         <AnimatePresence key={`toast-${toast.id}`}>
           {toast.visible && (
             <motion.div
               initial={{ opacity: 0, translateX: "100%" }}
               animate={{ opacity: 1, translateX: 0 }}
               exit={{ opacity: 0, translateX: "100%" }}
-              className="toast toast-top toast-end"
+              className={`toast toast-end`}
+              style={{ top: `${idx * 140}px` }}
             >
-              <div className="alert flex-col flex items-start">
+              <div className="alert flex-col flex items-start gap-1 lg:gap-2">
                 <div className="flex gap-2 items-center">
-                  {toast.variant === "success" && <FiCheckCircle size={24} />}
-                  {toast.variant === "error" && <FiXCircle size={24} />}
+                  {toast.variant === "success" && (
+                    <FiCheckCircle size={32} className="text-success" />
+                  )}
+                  {toast.variant === "error" && (
+                    <FiXCircle size={32} className="text-error" />
+                  )}
                   <span className="title mr-8">{toast.title}</span>
                 </div>
 
