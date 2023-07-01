e2ffd9d5c34b0eaa8616cd1c8bd0a19c3d4d6d9f
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-07-01 16:37:55 +0200

    Add week and date to status

--- pages/status.tsx
+++ pages/status.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from "next"
 import Link from "next/link"
+import { AiOutlineCalendar } from "react-icons/ai"
 import { BiFemaleSign, BiMaleSign, BiTime } from "react-icons/bi"
 import { GiCoins } from "react-icons/gi"
 
@@ -8,11 +9,14 @@ import Flag from "@/components/icons/Flag"
 import DefaultLayout from "@/components/layouts/default"
 import { useModal } from "@/components/ui/Modal/context"
 import { useGetPlayer } from "@/hooks/queries/usePlayer"
+import { getCurrentDate } from "@/utils/date"
 import { getLoggedInServerSideProps } from "@/utils/next/getLoggedInServerSideProps"
 
 const Status = () => {
   const { data: player } = useGetPlayer()
   const { setModal } = useModal()
+
+  const currentDate = getCurrentDate(player?.character.week || 0)
 
   const openCharacterEditModal = () => {
     setModal({
@@ -115,6 +119,26 @@ const Status = () => {
                 </div>
               </div>
             )}
+          </div>
+
+          <div className="stats gap-4 mt-4">
+            <div className="stat bg-gray-700">
+              <div className="stat-figure text-secondary">
+                <AiOutlineCalendar className="h-8 w-8" />
+              </div>
+              <div className="stat-title">Has been playing for</div>
+              <div className="stat-value text-xl">
+                {player?.character.week} weeks
+              </div>
+            </div>
+
+            <div className="stat bg-gray-700">
+              <div className="stat-figure text-secondary">
+                <AiOutlineCalendar className="h-8 w-8" />
+              </div>
+              <div className="stat-title">Current date</div>
+              <div className="stat-value text-xl">{currentDate}</div>
+            </div>
           </div>
 
           <div className="card-actions justify-end mt-4">
