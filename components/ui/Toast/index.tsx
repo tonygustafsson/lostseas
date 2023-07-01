089f8edb65b7217c1fda35fe563c21dae656a053
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-07-01 11:15:36 +0200

    New animation for toasts

--- components/ui/Toast/index.tsx
+++ components/ui/Toast/index.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, m as motion } from "framer-motion"
+import { AiOutlineCloseCircle } from "react-icons/ai"
 import { FiCheckCircle, FiXCircle } from "react-icons/fi"
 
 import { useToast } from "./context"
@@ -10,24 +12,33 @@ const Toast = () => {
   return (
     <>
       {Object.values(toasts).map((toast) => (
-        <div
-          key={`toast-${toast.id}`}
-          className={`toast toast-top toast-end ${toast.visible ? "open" : ""}`}
-        >
-          <div className="alert flex-col flex items-start">
-            <div className="flex gap-2 items-center">
-              {toast.variant === "success" && <FiCheckCircle size={24} />}
-              {toast.variant === "error" && <FiXCircle size={24} />}
-              <span className="title mr-8">{toast.title}</span>
-            </div>
+        <AnimatePresence key={`toast-${toast.id}`}>
+          {toast.visible && (
+            <motion.div
+              initial={{ opacity: 0, translateX: "100%" }}
+              animate={{ opacity: 1, translateX: 0 }}
+              exit={{ opacity: 0, translateX: "100%" }}
+              className="toast toast-top toast-end"
+            >
+              <div className="alert flex-col flex items-start">
+                <div className="flex gap-2 items-center">
+                  {toast.variant === "success" && <FiCheckCircle size={24} />}
+                  {toast.variant === "error" && <FiXCircle size={24} />}
+                  <span className="title mr-8">{toast.title}</span>
+                </div>
 
-            <span>{toast.message}</span>
-          </div>
+                <span>{toast.message}</span>
+              </div>
 
-          <button className="close" onClick={() => removeToast(toast.id || "")}>
-            ✕
-          </button>
-        </div>
+              <button
+                className="close"
+                onClick={() => removeToast(toast.id || "")}
+              >
+                <AiOutlineCloseCircle className="h-7 w-7" />
+              </button>
+            </motion.div>
+          )}
+        </AnimatePresence>
       ))}
     </>
   )
