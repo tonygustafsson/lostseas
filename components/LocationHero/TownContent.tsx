cfd7be1fd5afe2481ed62b54383d2df046d50d4e
Tony Gustafsson <mail@tonyg.se>
2023-07-05 21:26:04 +0200

    Change town hero

--- components/LocationHero/TownContent.tsx
+++ components/LocationHero/TownContent.tsx
@@ -13,13 +13,11 @@ const TownContent = ({ town, location }: Props) => {
 
   return (
     <>
-      <h1 className="font-serif mb-4 text-3xl lg:text-5xl">
-        {town}s {location}
-      </h1>
+      <h1 className="font-serif mb-4 text-3xl lg:text-5xl">The {location}</h1>
 
       <h2 className="font-serif mb-3 lg:mb-5 text-lg lg:text-2xl flex gap-3 justify-center items-center">
         <Flag nation={nation} className="w-5 h-5 lg:w-7 lg:h-7 opacity-[0.8]" />
-        {nation}
+        {town}, {nation}
       </h2>
 
       <p className="lg:mb-5 text-sm">{LOCATION_DESCRIPTION[location]}</p>
