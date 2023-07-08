58eced5f17c4173c0b975ee9a83e025541761a43
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-07-08 08:54:00 +0200

    Journey length is based on the distance

--- pages/api/sea/startJourney.ts
+++ pages/api/sea/startJourney.ts
@@ -2,6 +2,7 @@ import { getCookie } from "cookies-next"
 import { randomInt } from "crypto"
 import { NextApiRequest, NextApiResponse } from "next/types"
 
+import { TOWN_INFO } from "@/constants/locations"
 import { PLAYER_ID_COOKIE_NAME } from "@/constants/system"
 import { getCharacter, saveCharacter } from "@/firebase/db"
 
@@ -21,6 +22,12 @@ const seaStartJourney = async (req: NextApiRequest, res: NextApiResponse) => {
     return
   }
 
+  const currentTownInfo = character.town && TOWN_INFO[character.town]
+
+  const distance = currentTownInfo?.distanceTo?.[town]
+    ? currentTownInfo.distanceTo?.[town]
+    : randomInt(3, 9)
+
   const characterResult: Nullable<Character> = {
     ...character,
     town: null,
@@ -28,7 +35,7 @@ const seaStartJourney = async (req: NextApiRequest, res: NextApiResponse) => {
     journey: {
       destination: town,
       day: 1,
-      totalDays: randomInt(3, 9),
+      totalDays: distance,
     },
   }
 
