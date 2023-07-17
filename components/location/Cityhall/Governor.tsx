a1765fe988b9e32d0f80ea4e1b21faf467af390f
Tony Gustafsson <mail@tonyg.se>
2023-07-17 22:39:36 +0200

    Detect if you are in home nation when visiting governor

--- components/location/Cityhall/Governor.tsx
+++ components/location/Cityhall/Governor.tsx
@@ -1,3 +1,21 @@
-const Governor = () => <div>Governor</div>
+import { TOWNS } from "@/constants/locations"
+import { useGetPlayer } from "@/hooks/queries/usePlayer"
+
+const Governor = () => {
+  const { data: player } = useGetPlayer()
+
+  const townNation = player?.character.town
+    ? TOWNS[player?.character.town]?.nation
+    : null
+  const isInHomeNation = player?.character.nationality === townNation
+
+  return (
+    <>
+      <h2 className="text-2xl">Governor</h2>
+
+      {isInHomeNation && <p>Welcome {player.character.name}.</p>}
+    </>
+  )
+}
 
 export default Governor
