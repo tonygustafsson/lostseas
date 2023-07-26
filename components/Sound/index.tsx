6082275b7288a789d77514d84d5ab57814a9631b
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-07-26 14:46:00 +0200

    Bug fixes

--- components/Sound/index.tsx
+++ components/Sound/index.tsx
@@ -1,5 +1,4 @@
-import { observer } from "@legendapp/state/react"
-import { useCallback } from "react"
+import { useCallback, useEffect } from "react"
 
 import { useGetPlayer } from "@/hooks/queries/usePlayer"
 import { getRandomInt } from "@/utils/random"
@@ -9,7 +8,7 @@ import { sound$ } from "./state"
 const musicPlayer = typeof Audio !== "undefined" && new Audio()
 const songs = Array.from({ length: 9 }, (_, i) => `music/song${i + 1}.opus`)
 
-const Sound = observer(() => {
+const Sound = () => {
   const { data: player } = useGetPlayer()
 
   const playRandomSong = useCallback(() => {
@@ -23,51 +22,72 @@ const Sound = observer(() => {
     musicPlayer.play()
   }, [player])
 
-  if (!musicPlayer || !player) return
+  const disposeMusicPlayListener = sound$.musicPlay.onChange(
+    ({ value: musicPlay }) => {
+      if (!musicPlayer || !player) return
 
-  if (sound$.musicPlay.get() && !musicPlayer.src) {
-    playRandomSong()
+      if (!musicPlay) {
+        musicPlayer.pause()
+        musicPlayer.removeEventListener("ended", playRandomSong)
+        return
+      }
 
-    musicPlayer.addEventListener("ended", playRandomSong)
-  } else if (sound$.musicPlay.get() && musicPlayer.src) {
-    musicPlayer.play()
-  } else if (musicPlayer.src && !musicPlayer.paused) {
-    musicPlayer.pause()
-    musicPlayer.removeEventListener("ended", playRandomSong)
-  }
-
-  if (!sound$.soundEffectsOn.get() || !sound$.soundEffect.get()) return
-
-  let audioFile: string
-
-  if (sound$.soundEffect.get() === "journey") {
-    const wavesSoundEffects = ["soundfx/waves1.opus", "soundfx/waves2.opus"]
-
-    audioFile =
-      wavesSoundEffects[Math.floor(Math.random() * wavesSoundEffects.length)]
-
-    if (getRandomInt(1, 2) === 1) {
-      const journeyAdditionalSoundEffects = [
-        "soundfx/creak.opus",
-        "soundfx/seagulls.opus",
-      ]
-
-      const additionalAudioFile =
-        journeyAdditionalSoundEffects[
-          Math.floor(Math.random() * journeyAdditionalSoundEffects.length)
-        ]
-
-      const soundEffectPlayer = new Audio(additionalAudioFile)
-      soundEffectPlayer.play()
+      if (!musicPlayer.src) {
+        playRandomSong()
+        musicPlayer.addEventListener("ended", playRandomSong)
+      } else if (musicPlayer.src) {
+        musicPlayer.play()
+      }
     }
-  } else {
-    audioFile = `soundfx/${sound$.soundEffect.get()}.opus`
-  }
+  )
 
-  const soundEffectPlayer = new Audio(audioFile)
-  soundEffectPlayer.play()
+  const disposeSoundEffectListener = sound$.soundEffect.onChange(
+    ({ value: soundEffect }) => {
+      if (!sound$.soundEffectsOn.get() || !soundEffect) return
+
+      let audioFile: string
+
+      if (soundEffect === "journey") {
+        const wavesSoundEffects = ["soundfx/waves1.opus", "soundfx/waves2.opus"]
+
+        audioFile =
+          wavesSoundEffects[
+            Math.floor(Math.random() * wavesSoundEffects.length)
+          ]
+
+        if (getRandomInt(1, 2) === 1) {
+          const journeyAdditionalSoundEffects = [
+            "soundfx/creak.opus",
+            "soundfx/seagulls.opus",
+          ]
+
+          const additionalAudioFile =
+            journeyAdditionalSoundEffects[
+              Math.floor(Math.random() * journeyAdditionalSoundEffects.length)
+            ]
+
+          const soundEffectPlayer = new Audio(additionalAudioFile)
+          soundEffectPlayer.play()
+        }
+      } else {
+        audioFile = `soundfx/${soundEffect}.opus`
+      }
+
+      const soundEffectPlayer = new Audio(audioFile)
+      soundEffectPlayer.play()
+
+      soundEffectPlayer.onended = () => {
+        soundEffectPlayer.remove()
+      }
+    }
+  )
+
+  useEffect(() => () => {
+    disposeMusicPlayListener()
+    disposeSoundEffectListener()
+  })
 
   return null
-})
+}
 
 export default Sound
