e8842a3fb33f4bdaaef372fc086d47cdbf4e8fbf
Tony Gustafsson <mail@tonyg.se>
2023-08-02 22:26:00 +0200

    New shipyard backgrounds

--- components/LocationHero/index.tsx
+++ components/LocationHero/index.tsx
@@ -23,7 +23,11 @@ const LocationHero = () => {
     location: Character["location"],
     shipMeeting?: ShipMeetingState | null
   ) => {
-    if (["Shop", "Tavern", "Bank", "City hall", "Market"].includes(location)) {
+    if (
+      ["Shop", "Tavern", "Bank", "City hall", "Market", "Shipyard"].includes(
+        location
+      )
+    ) {
       const nation = getTownsNationality(town)
       return `/img/location/${location
         .replace(" ", "-")
