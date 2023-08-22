b3afa01a3756e7efea549b564652972a01a2ca83
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-08-22 10:12:41 +0200

    Unique market backgrounds

--- utils/location.ts
+++ utils/location.ts
@@ -32,7 +32,7 @@ export const getLocationBackground = (
     return `/img/location/sea/sea${randomImageNumber}.webp`
   }
 
-  if (["Shop", "Tavern", "Bank", "Shipyard"].includes(location)) {
+  if (["Shop", "Tavern", "Bank", "Market", "Shipyard"].includes(location)) {
     return `/img/location/${town?.toLowerCase().replace(" ", "-")}/${location
       .toLowerCase()
       .replace(" ", "-")}.webp`
@@ -51,7 +51,7 @@ export const getAllTownLocationBackgrounds = (town: Character["town"]) => {
   const images = Object.values(LOCATIONS)
     .filter((location) => location !== "Sea")
     .map((location) => {
-      if (["Shop", "Tavern", "Bank", "Shipyard"].includes(location)) {
+      if (["Shop", "Tavern", "Bank", "Market", "Shipyard"].includes(location)) {
         return `/img/location/${town
           ?.toLowerCase()
           .replace(" ", "-")}/${location.toLowerCase().replace(" ", "-")}.webp`
