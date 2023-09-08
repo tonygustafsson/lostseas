3d98c0edbbe71022d744f7ad235227c3a14c6cee
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2023-09-08 08:50:14 +0200

    Different music in town and on sea

--- components/Sound/index.tsx
+++ components/Sound/index.tsx
@@ -1,12 +1,21 @@
 import { useCallback, useEffect, useMemo } from "react"
 
+import { useGetPlayer } from "@/hooks/queries/usePlayer"
 import { getRandomInt } from "@/utils/random"
 
 import { useSound } from "./context"
 
-const songs = Array.from({ length: 9 }, (_, i) => `music/song${i + 1}.opus`)
+const townSongs = Array.from(
+  { length: 6 },
+  (_, i) => `music/town/song${i + 1}.opus`
+)
+const seaSongs = Array.from(
+  { length: 7 },
+  (_, i) => `music/sea/song${i + 1}.opus`
+)
 
 const Sound = () => {
+  const { data: player } = useGetPlayer()
   const { musicOn, soundEffectsOn, soundEffect } = useSound()
 
   const musicPlayer = useMemo(
@@ -14,16 +23,56 @@ const Sound = () => {
     []
   )
 
-  const playRandomSong = useCallback(() => {
+  const fadeOutMusic = () =>
+    new Promise((resolve) => {
+      if (!musicPlayer) {
+        resolve(true)
+        return
+      }
+
+      if (musicPlayer.src && !musicPlayer.paused) {
+        const fadeOutInterval = setInterval(() => {
+          if (musicPlayer.volume >= 0.05) {
+            musicPlayer.volume -= 0.05
+          } else {
+            musicPlayer.volume = 0
+            clearInterval(fadeOutInterval)
+            resolve(true)
+          }
+        }, 100)
+      } else {
+        resolve(true)
+      }
+    })
+
+  const playRandomSong = useCallback(async () => {
     if (!musicPlayer) return
 
+    await fadeOutMusic()
+
+    const songs = player?.character?.location === "Sea" ? seaSongs : townSongs
     const otherSongs = songs.filter((song) => song !== musicPlayer.src)
     const randomSong = otherSongs[Math.floor(Math.random() * otherSongs.length)]
 
     musicPlayer.src = randomSong
     musicPlayer.volume = 0.8
     musicPlayer.play()
-  }, [musicPlayer])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [musicPlayer, player?.character?.location])
+
+  useEffect(() => {
+    // Change music track if you are going out to sea or coming in to harbor
+    if (!musicPlayer) return
+
+    if (
+      (player?.character?.location === "Sea" &&
+        musicPlayer.src.includes("town")) ||
+      (player?.character?.location !== "Sea" && musicPlayer.src.includes("sea"))
+    ) {
+      playRandomSong()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [player?.character?.location])
 
   useEffect(() => {
     if (!musicPlayer) return
