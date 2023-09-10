9a849f5151b7f12742f3b9f253303d9137321f3a
Tony Gustafsson <mail@tonyg.se>
2023-09-10 21:57:07 +0200

    Random phrases on battle won/loss

--- components/LocationHero/AttackFailureContent.tsx
+++ components/LocationHero/AttackFailureContent.tsx
@@ -1,11 +1,34 @@
+const MESSAGES = [
+  "Amidst the deafening roar of enemy cannons and the ominous splintering of our once-proud mast, our valiant vessel succumbed to the relentless onslaught, its fate sealed on the unforgiving waters.",
+  "As our sails sagged and the relentless enemy onslaught continued, our gallant ship found itself overwhelmed, defeated by a foe whose determination proved insurmountable.",
+  "In the midst of chaos and despair, our majestic vessel succumbed to the merciless enemy's firepower, its once-mighty form now a battered and broken testament to our defeat on the open seas.",
+  "Despite our crew's unwavering resolve, our formidable ship faltered and fell before the relentless assault of the enemy, its tattered flag a symbol of our painful defeat at sea.",
+  "With our cannons silenced and our hull breached, our noble ship was vanquished by a foe whose cunning strategy left us no choice but to surrender on the unforgiving waters. ",
+  "Amidst the smoke and chaos, our grand vessel's defiance dwindled, ultimately yielding to the relentless adversary, our once-proud banner lowered in defeat.",
+  "Despite our valiant efforts, our illustrious ship was outmaneuvered and outgunned, forced to admit defeat as the enemy's triumphant cheers echoed across the open seas.",
+  "In the face of overwhelming odds, our gallant ship met its tragic end, the victorious enemy leaving no doubt as they claimed their supremacy on the high seas.",
+  "With our once-proud masts shattered and our crew in disarray, our formidable vessel was defeated by a relentless enemy, our colors struck in surrender on the open waters.",
+  "As the relentless enemy onslaught continued, our noble ship's fate was sealed, its once-undefeated crew humbled by the bitter taste of defeat on the unforgiving seas.",
+  "Despite our unwavering courage, our majestic ship could not withstand the unyielding assault of the enemy, its final moments marked by a resounding defeat at sea.",
+  "Amidst the chaos and devastation, our valiant vessel fell prey to the relentless enemy's superior strategy, its proud flag lowered in a heartbreaking defeat on the open waters.",
+  "With our cannons silenced and our sails in tatters, our gallant ship was overcome by the unyielding adversary, its once-proud crew forced to concede defeat on the unforgiving seas.",
+  "Despite our crew's fierce determination, our formidable vessel succumbed to the unrelenting enemy assault, our defeat at sea echoing across the waves.",
+  "In the face of overwhelming odds, our noble ship met its tragic end, the victorious enemy laying claim to their supremacy on the high seas.",
+  "With our once-proud mast shattered and our crew in disarray, our illustrious ship was defeated by the relentless enemy, our colors struck in surrender on the open waters.",
+  "Despite our valiant efforts, our majestic ship was outmaneuvered and outgunned, forced to admit defeat as the enemy's triumphant cheers echoed across the open seas.",
+  "As the relentless enemy onslaught continued, our grand vessel's defiance dwindled, ultimately yielding to the relentless adversary, our once-proud banner lowered in defeat.",
+  "Amidst the smoke and chaos, our formidable ship faltered and fell before the relentless assault of the enemy, its tattered flag a symbol of our painful defeat at sea.",
+  "Despite our crew's unwavering resolve, our gallant ship found itself overwhelmed, defeated by a foe whose determination proved insurmountable on the unforgiving waters.",
+]
+
+const randomMessage = MESSAGES[Math.floor(Math.random() * MESSAGES.length)]
+
 const AttackFailureContent = () => (
   <>
     <h1 className="font-serif mb-4 text-3xl lg:text-5xl">Battle lost!</h1>
 
     <div className="lg:mb-5 text-sm">
-      <p className="text-2xl mb-4 font-serif">
-        Ouch, that hurt! Let&apos;s lick our wounds and count our losses.
-      </p>
+      <p className="mb-4">{randomMessage} </p>
     </div>
   </>
 )
--- components/LocationHero/AttackSuccessContent.tsx
+++ components/LocationHero/AttackSuccessContent.tsx
@@ -1,12 +1,34 @@
+const MESSAGES = [
+  "Amidst billowing sails and roaring cannons, our majestic vessel outmaneuvered and outgunned the enemy fleet, securing a triumphant victory on the high seas.",
+  "Beneath the unforgiving sun, our mighty ship prevailed against the adversary's fleet, claiming victory amidst cannon fire and billowing sails.",
+  "With the wind in our favor and cannons ablaze, our gallant vessel triumphed over the opposing armada, asserting our dominance at sea.",
+  "Amidst the relentless waves, our formidable ship emerged victorious, its cannons roaring in triumph as the enemy fleet surrendered.",
+  "Through cunning tactics and fierce determination, our majestic vessel overcame the foe's armada, sealing our glorious conquest on the open waters.",
+  "As the battle raged under the vast expanse of the sky, our noble ship outmaneuvered and outgunned the enemy, securing a resounding triumph at sea.",
+  "In a symphony of chaos and valor, our grand vessel emerged victorious, its cannons thundering in triumph over the defeated enemy fleet.",
+  "Through skillful navigation and unwavering resolve, our illustrious ship vanquished the foe's armada, solidifying our supremacy on the high seas.",
+  "Amidst the tumultuous waves and cannon fire, our gallant vessel prevailed, its crew's unwavering courage leading us to a triumphant victory.",
+  "With the salt spray in our faces and the enemy in our sights, our majestic ship emerged victorious, its cannons roaring in defiance of the foe.",
+  "In a dramatic clash of sails and steel, our formidable vessel bested the adversary's fleet, securing a memorable triumph on the open waters.",
+  "In a dramatic maritime showdown, our illustrious ship outwitted and outgunned the enemy flotilla, emerging victorious amidst the thunderous roar of cannons and snapping sails.",
+  "Through a relentless barrage of cannon fire and the sheer might of our majestic vessel, we overcame the enemy fleet, securing an emphatic triumph on the open seas.",
+  "As the sea raged around us, our gallant ship's crew fought with unmatched valor, leading us to a glorious victory against the adversary's armada.",
+  "Amidst the rolling waves and plumes of smoke, our formidable vessel bested the opposing fleet, leaving no doubt as to our dominance on the high seas.",
+  "With unwavering resolve and a thunderous volley of cannon fire, our noble ship prevailed against the enemy, marking our triumph on the open waters.",
+  "In a heart-pounding clash of maritime might, our grand vessel emerged victorious, its cannons resounding in triumph over the defeated enemy flotilla.",
+  "Through a symphony of chaos and courage, our majestic ship overcame the foe's armada, securing our triumphant conquest on the open seas.",
+  "Amidst the salty spray and the enemy's desperate resistance, our valiant vessel prevailed, its cannons thundering in victory as the foe's colors struck.",
+  "With the wind in our favor and the enemy fleet in disarray, our illustrious ship sailed to triumph, its resounding victory echoing across the open waters.",
+]
+
+const randomMessage = MESSAGES[Math.floor(Math.random() * MESSAGES.length)]
+
 const AttackSuccessContent = () => (
   <>
     <h1 className="font-serif mb-4 text-3xl lg:text-5xl">Battle won!</h1>
 
     <div className="lg:mb-5 text-sm">
-      <p className="text-2xl mb-4 font-serif">
-        You have won the battle! Your crew cheers and you cannot help but feel a
-        bit proud of yourself.
-      </p>
+      <p className="mb-4">{randomMessage} </p>
     </div>
   </>
 )
