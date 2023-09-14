9d56cf242adb76e7e4f8b3b287d8387fb8b3cd90
Tony Gustafsson <mail@tonyg.se>
2023-09-14 21:36:00 +0200

    Report margins

--- components/location/AttackReport/index.tsx
+++ components/location/AttackReport/index.tsx
@@ -19,47 +19,55 @@ const AttackReport = () => {
       {successReport && (
         <ul className="flex flex-col gap-4">
           {successReport.foundTreasure && (
-            <li className="flex align-middle gap-2 text-lg">
+            <li className="flex align-middle gap-4 text-lg">
               <TreasureIcon
                 item={successReport.foundTreasure.name}
                 className="h-7 w-7 text-success"
               />
-              You found a treasure - the {successReport.foundTreasure.name}.
-              Rumors say it&apos;s worth{" "}
-              {
-                TREASURES.find(
-                  (treasure) =>
-                    successReport.foundTreasure &&
-                    treasure.name === successReport.foundTreasure?.name
-                )?.value
-              }{" "}
-              gold and that the governor of{" "}
-              {successReport.foundTreasure.rewarder} is looking for it.
+              <div className="flex-1">
+                You found a treasure - the {successReport.foundTreasure.name}.
+                Rumors say it&apos;s worth{" "}
+                {
+                  TREASURES.find(
+                    (treasure) =>
+                      successReport.foundTreasure &&
+                      treasure.name === successReport.foundTreasure?.name
+                  )?.value
+                }{" "}
+                gold and that the governor of{" "}
+                {successReport.foundTreasure.rewarder} is looking for it.
+              </div>
             </li>
           )}
 
           {successReport.lootedGold && (
-            <li className="flex align-middle gap-2 text-lg">
+            <li className="flex align-middle gap-4 text-lg">
               <FaCoins className="h-7 w-7 text-success" />
-              Your looted {successReport.lootedGold} gold and now have a total
-              of {player.character.gold} gold.
+              <div className="flex-1">
+                Your looted {successReport.lootedGold} gold and now have a total
+                of {player.character.gold} gold.
+              </div>
             </li>
           )}
 
           {successReport.crewMoodIncrease && (
-            <li className="flex align-middle gap-2 text-lg">
+            <li className="flex align-middle gap-4 text-lg">
               <GiBandana className="h-7 w-7 text-success" />
-              Your crews mood went up with {successReport.crewMoodIncrease}% and
-              is now at {player.crewMembers.mood}%.
+              <div className="flex-1">
+                Your crews mood went up with {successReport.crewMoodIncrease}%
+                and is now at {player.crewMembers.mood}%.
+              </div>
             </li>
           )}
 
           {!!successReport.crewMemberRecruits && (
-            <li className="flex align-middle gap-2 text-lg">
+            <li className="flex align-middle gap-4 text-lg">
               <FaUsers className="h-7 w-7 text-success" />
-              {successReport.crewMemberRecruits} crew members of the enemy ship
-              decided to join you, and you now have a total of{" "}
-              {player.crewMembers.count} crew members.
+              <div className="flex-1">
+                {successReport.crewMemberRecruits} crew members of the enemy
+                ship decided to join you, and you now have a total of{" "}
+                {player.crewMembers.count} crew members.
+              </div>
             </li>
           )}
 
@@ -73,37 +81,41 @@ const AttackReport = () => {
 
                 return (
                   <li
-                    className="flex align-middle gap-2 text-lg"
+                    className="flex align-middle gap-4 text-lg"
                     key={`looted-merchandise-report-${key}`}
                   >
                     <MerchandiseIcon
                       item={key as keyof Inventory}
                       className="text-success"
                     />
-                    {unit === "cannons" && <>You looted {value} cannons.</>}
-                    {unit !== "cannons" && (
-                      <>
-                        You looted {value} {unit && `${unit} of `} {key}.
-                      </>
-                    )}
+                    <div className="flex-1">
+                      {unit === "cannons" && <>You looted {value} cannons.</>}
+                      {unit !== "cannons" && (
+                        <>
+                          You looted {value} {unit && `${unit} of `} {key}.
+                        </>
+                      )}
+                    </div>
                   </li>
                 )
               }
             )}
 
           {successReport.crewHealthLoss && (
-            <li className="flex align-middle gap-2 text-lg">
+            <li className="flex align-middle gap-4 text-lg">
               <GiBandana className="h-7 w-7 text-error" />
-              Your crew lost {successReport.crewHealthLoss}% health, and now has
-              a health of {player?.crewMembers.health}%.
+              <div className="flex-1">
+                Your crew lost {successReport.crewHealthLoss}% health, and now
+                has a health of {player?.crewMembers.health}%.
+              </div>
             </li>
           )}
 
           {successReport.shipHealthLoss && (
-            <li className="flex align-middle gap-2 text-lg">
+            <li className="flex align-middle gap-4 text-lg">
               <GiShoonerSailboat className="h-7 w-7 text-error" />
 
-              <div>
+              <div className="flex-1">
                 Your ships lost {successReport.shipHealthLoss}% health. Your
                 ships now have the health of
                 <ul className="list-decimal list-inside">
@@ -123,40 +135,48 @@ const AttackReport = () => {
 
       {failureReport && (
         <ul className="flex flex-col gap-4">
-          <li className="flex align-middle gap-2 text-lg">
+          <li className="flex align-middle gap-4 text-lg">
             <FaCoins className="h-7 w-7 text-error" />
-            You lost all your gold. (Funds in bank are still safe)
+            <div className="flex-1">
+              You lost all your gold. (Funds in bank are still safe)
+            </div>
           </li>
 
-          <li className="flex align-middle gap-2 text-lg">
+          <li className="flex align-middle gap-4 text-lg">
             <GiOpenedFoodCan className="h-7 w-7 text-error" />
-            You lost{" "}
-            {failureReport.inventoryPercentageLoss === 100
-              ? "all"
-              : `${failureReport.inventoryPercentageLoss}%`}{" "}
-            of your inventory.
+            <div className="flex-1">
+              You lost{" "}
+              {failureReport.inventoryPercentageLoss === 100
+                ? "all"
+                : `${failureReport.inventoryPercentageLoss}%`}{" "}
+              of your inventory.
+            </div>
           </li>
 
           {failureReport.crewHealthLoss && (
-            <li className="flex align-middle gap-2 text-lg">
+            <li className="flex align-middle gap-4 text-lg">
               <GiBandana className="h-7 w-7 text-error" />
-              Your crew lost {failureReport.crewHealthLoss}% health, and now has
-              a health of {player.crewMembers.health}%.
+              <div className="flex-1">
+                Your crew lost {failureReport.crewHealthLoss}% health, and now
+                has a health of {player.crewMembers.health}%.
+              </div>
             </li>
           )}
 
           {failureReport.sunkShip && (
-            <li className="flex align-middle gap-2 text-lg">
+            <li className="flex align-middle gap-4 text-lg">
               <GiShoonerSailboat className="h-7 w-7 text-error" />
-              They sunk your ship {failureReport.sunkShip}.
+              <div className="flex-1">
+                They sunk your ship {failureReport.sunkShip}.
+              </div>
             </li>
           )}
 
           {failureReport.shipHealthLoss && (
-            <li className="flex align-middle gap-2 text-lg">
+            <li className="flex align-middle gap-4 text-lg">
               <GiShoonerSailboat className="h-7 w-7 text-error" />
 
-              <div>
+              <div className="flex-1">
                 Your ships lost {failureReport.shipHealthLoss}% health. Your
                 ships now have the health of
                 <ul className="list-decimal list-inside">
--- components/location/Harbor/index.tsx
+++ components/location/Harbor/index.tsx
@@ -15,68 +15,82 @@ const Harbor = () => {
     <>
       {journeyValidation && !journeyValidation.success && (
         <ParrotBox title="Yarr, not ready to leave yet">
-          <ul className="flex flex-col gap-3">
+          <ul className="flex flex-col gap-4">
             {journeyValidation.errors.includes("NO_SHIPS") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <GiShoonerSailboat className="h-7 w-7 text-primary" />
-                You do not own any ships.
+                <div className="flex-1">You do not own any ships.</div>
               </li>
             )}
 
             {journeyValidation.errors.includes("DAMAGED_SHIPS") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <GiShoonerSailboat className="h-7 w-7 text-primary" />
-                Some of your ships are too damaged to continue traveling. Fix
-                them in the shipyard.
+                <div className="flex-1">
+                  Some of your ships are too damaged to continue traveling. Fix
+                  them in the shipyard.
+                </div>
               </li>
             )}
 
             {journeyValidation.errors.includes("NOT_ENOUGH_CREW_MEMBERS") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <GiBandana className="h-7 w-7 text-primary" />
-                You need at least {journeyValidation.minCrew} crew members to
-                successfully sail your ships.
+                <div className="flex-1">
+                  You need at least {journeyValidation.minCrew} crew members to
+                  successfully sail your ships.
+                </div>
               </li>
             )}
 
             {journeyValidation.errors.includes("TOO_MANY_CREW_MEMBERS") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <GiBandana className="h-7 w-7 text-primary" />
-                Your ships can carry {journeyValidation.maxCrew} crew members at
-                most. Get a new ship or dismiss some crew members.
+                <div className="flex-1">
+                  Your ships can carry {journeyValidation.maxCrew} crew members
+                  at most. Get a new ship or dismiss some crew members.
+                </div>
               </li>
             )}
 
             {journeyValidation.errors.includes("ANGRY_CREW") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <GiBandana className="h-7 w-7 text-primary" />
-                Your crew is an angry bunch, they do not want to travel with you
-                any more. Make them happy by giving them gold or taking them to
-                the tavern.
+                <div className="flex-1">
+                  Your crew is an angry bunch, they do not want to travel with
+                  you any more. Make them happy by giving them gold or taking
+                  them to the tavern.
+                </div>
               </li>
             )}
 
             {journeyValidation.errors.includes("CREW_IS_ILL") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <GiBandana className="h-7 w-7 text-primary" />
-                Your crew health is too low, they are too ill to travel. Give
-                them medicine.
+                <div className="flex-1">
+                  Your crew health is too low, they are too ill to travel. Give
+                  them medicine.
+                </div>
               </li>
             )}
 
             {journeyValidation.errors.includes("NO_FOOD") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <MerchandiseIcon item="food" />
-                You do not have enough food for the journey. You need at least{" "}
-                {journeyValidation.neededFood} crates of food.
+                <div className="flex-1">
+                  You do not have enough food for the journey. You need at least{" "}
+                  {journeyValidation.neededFood} crates of food.
+                </div>
               </li>
             )}
 
             {journeyValidation.errors.includes("NO_WATER") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <MerchandiseIcon item="water" />
-                You do not have enough water for the journey. You need at least{" "}
-                {journeyValidation.neededWater} barrels of water.
+                <div className="flex-1">
+                  You do not have enough water for the journey. You need at
+                  least {journeyValidation.neededWater} barrels of water.
+                </div>
               </li>
             )}
           </ul>
@@ -85,61 +99,73 @@ const Harbor = () => {
 
       {landingTips && (
         <ParrotBox title="Heads up!">
-          <ul className="flex flex-col gap-3">
+          <ul className="flex flex-col gap-4">
             {landingTips.includes("TOO_MUCH_GOLD") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <FaCoins className="h-7 w-7 text-primary" />
-                You are carrying {player.character.gold} gold, it would be wise
-                to spend it on something useful or save it in the bank so you
-                won&apos;t loose it if you are defeated at sea.
+                <div className="flex-1">
+                  You are carrying {player.character.gold} gold, it would be
+                  wise to spend it on something useful or save it in the bank so
+                  you won&apos;t loose it if you are defeated at sea.
+                </div>
               </li>
             )}
 
             {landingTips.includes("DAMAGED_SHIPS") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <GiShoonerSailboat className="h-7 w-7 text-primary" />
-                Some of your ships are too damaged to continue traveling. Fix
-                them in the shipyard.
+                <div className="flex-1">
+                  Some of your ships are too damaged to continue traveling. Fix
+                  them in the shipyard.
+                </div>
               </li>
             )}
 
             {landingTips.includes("NO_CREW") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <GiBandana className="h-7 w-7 text-primary" />
-                You do not have any crew members.
+                <div className="flex-1">You do not have any crew members.</div>
               </li>
             )}
 
             {landingTips.includes("ANGRY_CREW") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <GiBandana className="h-7 w-7 text-primary" />
-                Your crew is an angry bunch, they do not want to travel with you
-                any more. Make them happy by giving them gold or taking them to
-                the tavern.
+                <div className="flex-1">
+                  Your crew is an angry bunch, they do not want to travel with
+                  you any more. Make them happy by giving them gold or taking
+                  them to the tavern.
+                </div>
               </li>
             )}
 
             {landingTips.includes("CREW_IS_ILL") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <GiBandana className="h-7 w-7 text-primary" />
-                Your crew health is too low, they are too ill to travel. Give
-                them medicine.
+                <div className="flex-1">
+                  Your crew health is too low, they are too ill to travel. Give
+                  them medicine.
+                </div>
               </li>
             )}
 
             {landingTips.includes("NEED_MORE_FOOD") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <MerchandiseIcon item="food" />
-                You will probably need more food in stock in order to make the
-                next journey.
+                <div className="flex-1">
+                  You will probably need more food in stock in order to make the
+                  next journey.
+                </div>
               </li>
             )}
 
             {landingTips.includes("NEED_MORE_WATER") && (
-              <li className="flex align-middle gap-2 text-lg">
+              <li className="flex align-middle gap-4 text-lg">
                 <MerchandiseIcon item="water" />
-                You will probably need more water in stock in order to make the
-                next journey.
+                <div className="flex-1">
+                  You will probably need more water in stock in order to make
+                  the next journey.
+                </div>
               </li>
             )}
           </ul>
