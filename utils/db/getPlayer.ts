7cd30585da4e23ed9eac71dc533e7edfa8684a58
Tony Wendel <twendel@sqli.com>
2026-03-20 10:20:02 +0100

    Fix cache

--- utils/db/getPlayer.ts
+++ utils/db/getPlayer.ts
@@ -1,17 +1,25 @@
+import { cache } from "react"
+
 import { getPlayer as getPlayerFromDatabase } from "@/firebase/db"
 
 import { sortShipsByDate } from "../sort"
+
+const getPlayerFromDatabaseCached = cache(async (playerId: Player["id"]) =>
+  getPlayerFromDatabase(playerId)
+)
 
 export const getPlayer = async (playerId: Player["id"] | undefined) => {
   if (!playerId) {
     return null
   }
 
-  const player = await getPlayerFromDatabase(playerId)
+  const rawPlayer = await getPlayerFromDatabaseCached(playerId)
 
-  if (!player) {
+  if (!rawPlayer) {
     return null
   }
+
+  const player = structuredClone(rawPlayer)
 
   player.id = playerId
   player.ships = sortShipsByDate<Ship>(player.ships) ?? {}
