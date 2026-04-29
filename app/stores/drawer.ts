1940aa511ee8caa0ddf1260f1dc9f566b2571cd5
Tony Wendel <twendel@sqli.com>
2026-04-29 20:54:25 +0200

    Animate between drawers

--- app/stores/drawer.ts
+++ app/stores/drawer.ts
@@ -20,7 +20,15 @@ type DrawerStore = {
 
 const useDrawer = create<DrawerStore>((set) => ({
   active: null,
-  open: (drawer) => set({ active: drawer }),
+  open: (drawer) => {
+    if (!!useDrawer.getState().active) {
+      // Close and open it after 0.1 second so that you can animate a transition from one drawer to another
+      useDrawer.getState().close()
+      setTimeout(() => set({ active: drawer }), 100)
+    } else {
+      set({ active: drawer })
+    }
+  },
   close: () => set({ active: null }),
 }))
 
